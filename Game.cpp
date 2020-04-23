86380f633b464373ef89805b02d6fe6bacc9aa8e
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-23 15:19:55 +0900

    rewrite in Camel case

--- Game.cpp
+++ Game.cpp
@@ -2,15 +2,17 @@
 #include <Windows.h>
 #include <thread>
 #include <chrono>
+#include <memory>
+
 
 #include "message/Message.h"
-#include "window/GameWindow.h"
+#include "window/window.h"
 
 using std::cout;
 using std::endl;
 
 //game
-void game() {
+void StartGame() {
 	
 	cout << "Game Start!" << endl;
 }
@@ -19,13 +21,17 @@ void game() {
 //main
 int main() {
 	Message me;
+	window wi;
+	int a = wi.getWindowSize();
+
 
 	//me.showErrorMessage("");
 	
 	//cout << "HelloWOrld!" << endl;
 	
 	//ƒQ[ƒ€ŠJŽn
-	game();
+	StartGame();
+	
 
 	return 0;
 }
