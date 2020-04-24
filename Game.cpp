acea02fec57a5aedf07bc7b0eb113bc352e9cf92
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-24 19:43:06 +0900

    GameBoard,Score,DrawGameBoardヘッダーの追加

--- Game.cpp
+++ Game.cpp
@@ -3,18 +3,24 @@
 #include <thread>
 #include <chrono>
 #include <memory>
+#include <conio.h>
 
 
 #include "message/Message.h"
 #include "window/window.h"
+#include "state/Score.h"
+#include "board/GameBoard.h"
+#include "board/DrawGameBoard.h"
 
 using std::cout;
 using std::endl;
 
+
+
 //game
 void StartGame() {
-	
-	cout << "Game Start!" << endl;
+		
+	//cout << "Game Start!" << endl;
 }
 
 
@@ -22,16 +28,21 @@ void StartGame() {
 int main() {
 	Message me;
 	window wi;
-	int a = wi.getWindowSize();
+	Score st;
+	GameBoard gb;
+	DrawGameBoard dgb;
 
-
-	//me.showErrorMessage("");
+	ƒQ[ƒ€ŠJŽn
 	
-	//cout << "HelloWOrld!" << endl;
-	
-	//ƒQ[ƒ€ŠJŽn
 	StartGame();
-	
+	gb.InitGameBoard();
+	dgb.DrawBoard();
+	gb.DrawStage();
+	dgb.DrawScore();
+
+	ˆêŽž’âŽ~
+	_getch();
+	cout << st.GetScore() << endl;
 
 	return 0;
 }
