bc8550703d836ff3e9e4fa4efb963b4aaa435262
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-24 11:57:53 +0900

    rewite GameBoard

--- board/GameBoard.cpp
+++ board/GameBoard.cpp
@@ -6,15 +6,15 @@ typedef unsigned char byte;
 using std::cout;
 using std::endl;
 
-// 盤面初期化
+// ボード初期化
 void GameBoard::init_board() {
-    // 盤面の中身を全て空に
+
     for (int x = 1; x < cGbWidth + 1; ++x) {
         for (int y = 1; y < cGbHeight + 1; ++y) {
-            g_board[x][y] = cEmpty;     //  盤面を空に
+            g_board[x][y] = cEmpty;    
         }
     }
-    // 外枠部分に WALL を設定
+  
     for (int x = 0; x < cGbWidth + 2; ++x) {
         g_board[x][0] = g_board[x][cGbHeight + 1] = WALL;
     }
