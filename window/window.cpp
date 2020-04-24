b3818ad948edb193ca697c02c76e5790837edfc8
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-24 19:42:00 +0900

    WInAPIを用いたウィンドウの生成テスト

---
+++ window/window.cpp
@@ -0,0 +1,34 @@
+#include "window.h"
+#include <memory>
+#include <iostream>
+#include <cstdlib>
+
+//int window::getWidth() {
+//	return w_width;
+//}
+//
+//void window::setWidth(int w) {
+//	this->w_width;
+//}
+//
+//int window::getHeight() {
+//	return w_height;
+//}
+//
+//void window::setHeight(int h) {
+//	this->w_height;
+//}
+
+//int window::getWindowSize() {
+//	//unique_ptr<int[]> ws(new int[2] {getWidth(),getHeight()});
+//	int ws = 0;
+//	return ws;
+//}
+//
+//void window::setWindowSize(int ws[2]) {
+//	ws[0] = getWidth();
+//	ws[1] = getHeight();
+//}
+
+
+
