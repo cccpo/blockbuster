db20e2c0d697584fb2c7b4656bf9412c2c559866
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-28 10:08:04 +0900

    Game.cpp及びGame.hの不必要な宣言などを削除

--- Game.cpp
+++ Game.cpp
@@ -9,9 +9,9 @@
 //#include "message/Message.h"
 //#include "window/window.h"
 //#include "state/Score.h"
-//#include "board/GameBoard.h"
-//#include "board/DrawGameBoard.h"
-//#include "agent/BlockPiece.h"
+#include "board/GameBoard.h"
+#include "board/DrawGameBoard.h"
+#include "agent/BlockPiece.h"
 
 using std::cout;
 using std::endl;
@@ -20,31 +20,19 @@ using std::endl;
 
 //game
 void StartGame() {
-	cout << "徹" << endl;
 	
-	/*Message me;*/
-	//window wi;
-	//Score st;
-	//GameBoard gb;
+	GameBoard gb;
+	DrawGameBoard dgb;
+	BlockPiece bp;
 
-	//DrawGameBoard dgb;
-	//BlockPiece bp;
 
-	//gScore = { 0 };
+	gb.InitGameBoard();//ゲームボード初期化
+	gb.DrawBoard();//ゲームボード外枠の描画(ToDo)
+	gb.DrawStage();//ゲームボード内部の描画(ToDo)
+	dgb.DrawScore();//スコア表示(ToDO)
+	bp.SetTertimino();
+	bp.DrawTetrimino();
 
-	//gb.InitGameBoard();//ゲームボード初期化
-	//gb.DrawBoard();//ゲームボード外枠の描画(ToDo)
-	//gb.DrawStage();//ゲームボード内部の描画(ToDo)
-	//dgb.DrawScore();//スコア表示(ToDO)
-	//bp.SetTertimino();
-	//bp.DrawTetrimino();
-	//int tpy = 0;
-	//while (gTetriminoPosY < gb.GetcGbHeight()) {
-	//	Sleep(100);
-	//	++tpy;
-	//	gb.DrawStage();//ゲームボード外枠の描画(ToDo)
-	//	bp.DrawTetrimino();
-	//}
 
 }
 
--- Game.h
+++ Game.h
@@ -1,23 +1,10 @@
 #pragma once
 
-
-//extern int gTetriminoType;
-//extern int gTetriminoPosX;
-//extern int gTetriminoPosY;
-//extern int sScore;
-extern 
-
 class Game
 {
+private:
 
 public:
 	
-	//void GetTetriminoType();
-
-	//void GetTetriminoPosX();
-
-	//void GetTetriminoPosY();
-
-	//void GetScore();
 };
 
