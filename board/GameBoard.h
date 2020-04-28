357dcd2106a15894ff5c85b8b0ffc32867d0e78f
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-28 12:00:09 +0900

    色の宣言をマクロから列挙型"enum class"に変更

--- board/GameBoard.h
+++ board/GameBoard.h
@@ -1,54 +1,51 @@
 #pragma once
 #include <iostream>
+#include <Windows.h>
 
 typedef unsigned char byte;
 
-extern const int cGbWidth = { 10 };
-extern const int cGbHeight = { 20 };
+const int cGbWidth = 10;
+const int cGbHeight = 20;
+
+
+
+enum class Mask {
+	
+};
 
 class GameBoard
 {	
 
 private:
-#define		COL_BLACK  0x00
-#define		COL_DARK_BLUE		0x01
-#define		COL_DARK_GREEN	0x02
-#define		COL_DARK_CYAN		0x03
-#define		COL_DARK_RED		0x04
-#define		COL_DARK_VIOLET	0x05
-#define		COL_DARK_YELLOW	0x06
-#define		COL_GRAY	0x07
-#define		COL_LIGHT_GRAY		0x08
-#define		COL_BLUE		0x09
-#define		COL_GREEN	0x0a
-#define		COL_CYAN		0x0b
-#define		COL_RED		0x0c
-#define		COL_VIOLET	0x0d
-#define		COL_YELLOW	0x0e
-#define		COL_WHITE	0x0f
-#define		COL_INTENSITY		0x08		//	çÇãPìxÉ}ÉXÉN
-#define		COL_RED_MASK		0x04
-#define		COL_GREEN_MASK	0x02
-#define		COL_BLUE_MASK	0x01
+	const static int cOuterFrame = 0xff;
+
+
+
+	enum Mask {
+		Intensity=8,RedMask=4,GreenMask=2,BlueMask=1
+	};
+
 
 	public:
 	const int cEmpty = { 0 };
 
 	byte gBoard[cGbWidth + 2][cGbHeight + 2]; 
 
+	enum class Color
+	{
+		Black, DarkBlue, DarkGreen, DarkCyan, DarlRed, DarkViolet, DarkYellow, Gray, LightGray, Blue,
+		Green, Cyan, Red, Violet, Yellow, White
 
-	
-	const static int OuterFrame = 0xff;
-
+	};
 
 	const static int cGameBoardPosX = 1; //
 	const static int cGameBoardPosY = 2;
 
 
 
-	void InitGameBoard();
-	void DrawStage();
-	void DrawBoard();
+	void InitGameBoard();	
+	void DrawBoard();//ÉQÅ[ÉÄÉ{Å[ÉhäOògÇÃï`âÊ(ToDO DrawGameBoardÇ…à⁄êAó\íË)
+	void DrawStage();//ÉQÅ[ÉÄÉ{Å[Éhì‡ïîÇÃï`âÊ(ToDO DrawGameBoardÇ…à⁄êAó\íË)
 
 	void SetColor(int fg, int bg);
 	void SetCursorPos(int x, int y);
@@ -59,7 +56,10 @@ private:
 
 	int GetcGbHeight();
 
+	int GetcGameBoardPosX();
 
+	int GetcGameBoardPosY();
+	
 
 	/*byte GetgBoard(int );*/
 
