dda43b40d5f1be802654fc646090f6307a1c7140
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-04-28 12:06:54 +0900

    色管理クラス"ColorManagement"の生成。

---
+++ Color/ColorManagement.h
@@ -0,0 +1,13 @@
+#pragma once
+enum class Color
+{
+	Black, DarkBlue, DarkGreen, DarkCyan, DarlRed, DarkViolet, DarkYellow, Gray, LightGray, Blue,
+	Green, Cyan, Red, Violet, Yellow, White
+};
+
+class ColorManagement
+{
+
+
+};
+
