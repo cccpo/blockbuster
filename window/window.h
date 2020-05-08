1f7f683b0841074759b19f7a5cff954b212839fb
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-05-08 19:18:04 +0900

    WinAPIを用いてウィンドウ生成()

---
+++ window/window.h
@@ -0,0 +1,19 @@
+#pragma once
+class window
+{
+ public:
+
+	typedef unsigned char byte;
+	//const static int w_width = 10;			//	盤面サイズ（外枠を除く）
+	//const static int w_height = 20;
+	//
+	//int getWidth();
+	//void setWidth(int w);
+
+	//int getHeight();
+	//void setHeight(int h);
+
+	//int getWindowSize();
+	//void setWindowSize(int ws[2]);
+};
+
