6b5828b8e66e06ddae0f393e1ff5648641d1b7aa
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-05-12 13:47:14 +0900

    関数に説明を追加

--- Data/Data.cpp
+++ Data/Data.cpp
@@ -3,15 +3,16 @@
 static int HiScore;
 
 
-
+//"HiScore.txt"よりハイスコアを取得
 int Data::HiScoreLoad() {
     std::ifstream ifs("HiScore.txt");
     std::string str;
     
-
+    //取得できない場合は異常終了
     if (ifs.fail()) {
         return -1;
     }
+    //一行取得 #ToDo ランキングを設けるのであればハイスコアテーブルを設けて実装する
     std::getline(ifs, str);
 
     HiScore = atoi(str.c_str());
@@ -19,6 +20,7 @@ int Data::HiScoreLoad() {
     return (HiScore);
 }
 
+//ハイスコアを"HiScore.txt"に保存する
 void Data::HiScoreSave(int sc) {
     std::ofstream outputfile("HiScore.txt");
     outputfile << sc;
