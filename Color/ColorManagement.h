866e39964288eac8740a9cf39292251bf0099678
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-05-12 18:29:23 +0900

    GameBoardクラスより色の管理を行う関数を移植

--- Color/ColorManagement.h
+++ Color/ColorManagement.h
@@ -1,13 +1,16 @@
 #pragma once
-enum class Color
-{
-	Black, DarkBlue, DarkGreen, DarkCyan, DarlRed, DarkViolet, DarkYellow, Gray, LightGray, Blue,
-	Green, Cyan, Red, Violet, Yellow, White
-};
+
 
 class ColorManagement
 {
 
+public:
+	enum class Color
+	{
+		Black = 0x00, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkViolet, DarkYellow, Gray, LightGray, Blue,
+		Green, Cyan, Red, Violet, Yellow, White
+	};
 
+	void SetColor(int fg, int bg);
 };
 
