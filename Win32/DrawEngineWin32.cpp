e135ba4eabb94da9dfbe66c171edda7a5d2bd4dc
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-06-30 12:55:32 +0900

    UI表示部の追加

--- Win32/DrawEngineWin32.cpp
+++ Win32/DrawEngineWin32.cpp
@@ -1,6 +1,7 @@
 #include "DrawEngineWin32.h"
 
-DrawEngineWin32::DrawEngineWin32(HDC mHdc, HWND mHwnd, int pxPerBlock = 15, int mWidth = 10, int mHeight = 20) :
+DrawEngineWin32::DrawEngineWin32(HDC mHdc, HWND mHwnd, 
+	int pxPerBlock = 15, int mWidth = 10, int mHeight = 20) :
 	mHdc(mHdc), mHwnd(mHwnd), mWidth(mWidth), mHeight(mHeight)
 {
 	//クライアントエリアのサイズを取得
@@ -15,11 +16,27 @@ DrawEngineWin32::DrawEngineWin32(HDC mHdc, HWND mHwnd, int pxPerBlock = 15, int 
 	SetViewportOrgEx(mHdc, 0, mRect.bottom, 0);
 
 	SetTextColor(mHdc, RGB(255, 255, 255));
-	SetBkColor(mHdc, RGB(70, 70, 70));
-	SetBkMode(mHdc, TRANSPARENT);
+	SetBkColor(mHdc, RGB(70, 70, 70));//背景色の指定
+	SetBkMode(mHdc, TRANSPARENT);//背景モードを変更
 }
 
 DrawEngineWin32::~DrawEngineWin32()
 {
 	RestoreDC(mHdc, -1);
 }
+
+//UIの表示
+void DrawEngineWin32::DrawUI() 
+{
+	// Draw a gray area at the right
+	HBRUSH hBrush = CreateSolidBrush(RGB(70, 70, 70));
+	
+	mRect.top = mHeight;
+	mRect.left = mWidth;
+	mRect.bottom = 0;
+	mRect.right = mWidth + 8;
+
+	FillRect(mHdc, &mRect, hBrush);
+	DeleteObject(hBrush);
+}
+
