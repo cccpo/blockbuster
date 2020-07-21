7e60ed7af8fe4c815e53af2da1fa8dfee0469661
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-07-21 11:36:02 +0900

    TetriminoSet.cpp:ルールに従い、色を修正

--- Win32/TetriminoSet.cpp
+++ Win32/TetriminoSet.cpp
@@ -8,54 +8,58 @@ TetriminoSet::TetriminoSet()
 
     POINT tetrimino_constituion[Rotate_Pattern];
 
-    //
+    //I,水色(aqua)
     tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
     tetrimino_constituion[1].x = 0;    tetrimino_constituion[1].y = 1;
     tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 2;
     tetrimino_constituion[3].x = 0;    tetrimino_constituion[3].y = 3;
-    tetriminos[0][0] = new TetriminoWin32(0, 0, RGB(255, 0, 0), tetrimino_constituion);
+    tetriminos[0][0] = new TetriminoWin32(0, 0, RGB(0, 255, 255), tetrimino_constituion);
 
-    //
-    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
-    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
-    tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 1;
-    tetrimino_constituion[3].x = 0;    tetrimino_constituion[3].y = 2;
-    tetriminos[1][0] = new TetriminoWin32(1, 0, RGB(230, 130, 24), tetrimino_constituion);
-
-    //
-    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
-    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
-    tetrimino_constituion[2].x = 1;    tetrimino_constituion[2].y = 1;
-    tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 2;
-    tetriminos[2][0] = new TetriminoWin32(2, 0, RGB(255, 255, 0), tetrimino_constituion);
-
-    // 
-    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
-    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
-    tetrimino_constituion[2].x = 1;    tetrimino_constituion[2].y = 1;
-    tetrimino_constituion[3].x = 2;    tetrimino_constituion[3].y = 1;
-    tetriminos[3][0] = new TetriminoWin32(3, 0, RGB(120, 200, 80), tetrimino_constituion);
-
-    // 
-    tetrimino_constituion[0].x = 1;    tetrimino_constituion[0].y = 0;
-    tetrimino_constituion[1].x = 2;    tetrimino_constituion[1].y = 0;
-    tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 1;
-    tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 1;
-    tetriminos[4][0] = new TetriminoWin32(4, 0, RGB(100, 180, 255), tetrimino_constituion);
-
-    //
+    //O,黄色(yellow)
     tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
     tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
     tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 1;
     tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 1;
-    tetriminos[5][0] = new TetriminoWin32(5, 0, RGB(20, 100, 200), tetrimino_constituion);
+    tetriminos[1][0] = new TetriminoWin32(1, 0, RGB(255, 255, 0), tetrimino_constituion);
 
-    // 
+    // T,紫色(purple)
     tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
     tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
     tetrimino_constituion[2].x = 2;    tetrimino_constituion[2].y = 0;
     tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 1;
-    tetriminos[6][0] = new TetriminoWin32(6, 0, RGB(220, 180, 255), tetrimino_constituion);
+    tetriminos[2][0] = new TetriminoWin32(2, 0, RGB(128, 0, 128), tetrimino_constituion);
+
+    //J,青色(blue)
+    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
+    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
+    tetrimino_constituion[2].x = 1;    tetrimino_constituion[2].y = 1;
+    tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 2;
+    tetriminos[3][0] = new TetriminoWin32(3, 0, RGB(0, 0, 255), tetrimino_constituion);
+
+    //L,オレンジ色(orange)
+    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
+    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
+    tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 1;
+    tetrimino_constituion[3].x = 0;    tetrimino_constituion[3].y = 2;
+    tetriminos[4][0] = new TetriminoWin32(4, 0, RGB(255, 165, 0), tetrimino_constituion);
+
+    // S,黄緑色(lime)
+    tetrimino_constituion[0].x = 0;    tetrimino_constituion[0].y = 0;
+    tetrimino_constituion[1].x = 1;    tetrimino_constituion[1].y = 0;
+    tetrimino_constituion[2].x = 1;    tetrimino_constituion[2].y = 1;
+    tetrimino_constituion[3].x = 2;    tetrimino_constituion[3].y = 1;
+    tetriminos[5][0] = new TetriminoWin32(5, 0, RGB(0, 255, 0), tetrimino_constituion);
+
+    //Z,赤色(red) 
+    tetrimino_constituion[0].x = 1;    tetrimino_constituion[0].y = 0;
+    tetrimino_constituion[1].x = 2;    tetrimino_constituion[1].y = 0;
+    tetrimino_constituion[2].x = 0;    tetrimino_constituion[2].y = 1;
+    tetrimino_constituion[3].x = 1;    tetrimino_constituion[3].y = 1;
+    tetriminos[6][0] = new TetriminoWin32(6, 0, RGB(255, 0, 0), tetrimino_constituion);
+
+
+
+
 
     MakeTetriminoRotateAllPattern();
 }
